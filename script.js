5ce614467ac8a717757f7ae4198cd34bd636da0a
truyenho95 <truyenho.webdev@gmail.com>
2018-11-12 14:52:33 +0700

    Add index.html script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+var x = prompt('Mời nhập mảng');
+if (x=='') 
+  var arr = [];
+else 
+  var arr = x.split(',');
+
+function minArray(arr) {
+
+  document.write('Mảng bạn nhập vào là: <br>');
+  for (var i=0; i<arr.length; i++)
+    document.write((i===arr.length-1) ? (arr[i]+'<br>') : (arr[i]+', '));
+
+  if(arr.length == 0)
+      return -1;
+
+  var min = arr[0];
+
+
+  for(var i = 1; i < arr.length; i++){
+    if(arr[i] < min){
+      min = arr[i];
+    }
+  }
+  return min;
+}
+
+window.onload = () => {
+  var min = minArray(arr);
+  document.write("min: " + min);
+}
